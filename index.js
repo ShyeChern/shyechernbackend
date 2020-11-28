48ac205e40106af230dd285477658809c4ccdb45
Shye Chern Lim <46220892+ShyeChern@users.noreply.github.com>
2020-11-28 23:51:30 +0800

    test chg

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`))
-
-
 // const express = require('express')
-// const app = express();
-// const port = 8000;
+// const path = require('path')
+// const PORT = process.env.PORT || 5000
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// });
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .listen(PORT, () => console.log(`Listening on ${PORT}`))
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}!`)
-// });
+
+const express = require('express')
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+});
