f2d0413e84247375311e44cfb1b2489e2b15d8f2
Shye Chern Lim <46220892+ShyeChern@users.noreply.github.com>
2020-11-29 11:16:32 +0800

    test

---
+++ app/database.js
@@ -0,0 +1,46 @@
+const { MongoClient } = require('mongodb');
+
+require('dotenv').config();
+
+module.exports.main = async () => {
+  /**
+   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+   */
+
+  // mongodb+srv://chern_1997:<password>@shyecherncluster.nvwz2.mongodb.net/<dbname>?retryWrites=true&w=majority
+
+  const uri = `mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@${process.env.DBURL}/<dbname>?\
+                retryWrites=true&w=majority`;
+  console.log(uri);
+  const client = new MongoClient(uri);
+
+  try {
+    // Connect to the MongoDB cluster
+    await client.connect();
+
+    // Make the appropriate DB calls
+    // await listDatabases(client);
+
+    databasesList = await client.db().admin().listDatabases();
+
+    // console.log("Databases:");
+    let data = [];
+    databasesList.databases.forEach(db => data.push(`${db.name}`));
+
+    return data;
+
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
+
+async function listDatabases(client) {
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // const express = require('express')
 // const path = require('path')
+// require('dotenv').config();
 // const PORT = process.env.PORT || 5000
 
 // express()
@@ -13,11 +14,40 @@
 const express = require('express')
 require('dotenv').config();
 const app = express();
+const path = require('path')
 const port = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// db
+var db = require('./app/database');
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ limit: '200mb', extended: true }));
+app.use(bodyParser.json({ limit: '200mb', extended: true }));
+
+app.use(function (req, res, next) {
+  // website
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-with,content-type');
+
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
 });
+
+app.use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs').
+  get('/', async (req, res) => {
+    // res.send('Hello World!');
+    let data = await db.main();
+    res.render('pages/index', {
+      data: data
+    })
+    // res.send(data);
+    res.end();
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
