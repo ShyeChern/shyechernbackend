09c2b2a9ddc3d5bbef6ae911a17babd4f8ee591a
Shye Chern Lim <46220892+ShyeChern@users.noreply.github.com>
2020-12-22 22:24:31 +0800

    test

--- config/routes.js
+++ config/routes.js
@@ -13,9 +13,6 @@ module.exports.setRoutes = (app) => {
     // api route, all the api will start with /api/xx
     app.use('/api', apiRoutes);
 
-    // upload directory
-    app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
-
     // send 404 error if other path
     app.use('/*', (req, res) => {
         res.status(404).send({ result: false, message: 'Requested path does not exist' });
